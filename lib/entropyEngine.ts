a1173902ed77c5afe8578ba8be5974acb0284463
Clayton <receiptx86@gmail.com>
2025-11-22 10:39:21 -0700

    chore: allow proprietary stubs for Netlify build

---
+++ lib/entropyEngine.ts
@@ -0,0 +1,5 @@
+// Build-safe stub for Netlify/CI
+export class EntropyEngine {
+  constructor() {}
+  getEntropyBuffer() { return Buffer.from('stub'); }
+}
---
+++ lib/proprietary/web2web3Bridge.ts
@@ -0,0 +1,5 @@
+// Build-safe stub for Netlify/CI
+export class Web2Web3Bridge {
+  constructor() {}
+  // Add stub methods as needed for build
+}
